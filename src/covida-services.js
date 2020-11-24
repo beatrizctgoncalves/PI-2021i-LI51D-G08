c0f48868b94b77a094af85b3449a5962736f8e90
diogotag@gmail.com <ZxS1rN4ndez2>
2020-11-24 19:33:56 +0000

    added unitary test

--- src/covida-services.js
+++ src/covida-services.js
@@ -2,33 +2,59 @@
 
 const urllib = require('urllib')
 
-const LASTFM_HOST = 'https://api.igdb.com/v4/'
-const LASTFM_KEY = '79b2506be8ce86d852882e1774f1f2e8'
-const LASTFM_TOP_TRACKS = `?method=artist.gettoptracks&format=json&api_key=${LASTFM_KEY}&artist=`
+const IGDB_HOST = 'https://api.igdb.com/v4/games'
+const IGDB_KEY = 's4fwgb8isqexk2j87n2xagqfc3hhy6'
+const IGDB_TOP_GAMES = `fields *;`
 
-/**
- * @param {String} artist Name of the band or artist.
- * @param {function(Error, Array)} cb Callback receiving an array with tracks names or Error if not succeeded
- */
-function getTopTracks(artist, cb) {
-    const path = LASTFM_HOST + LASTFM_TOP_TRACKS + artist
-    urllib.request(path, (err, data, res) => {
-        if(err) return cb(err)
-        const obj = JSON.parse(data)
-        cb(null, obj.toptracks.track.map(t => t.name))
-    })
+var request = {
+    host = 'https://api.igdb.com/v4/games',
+    key = '5tfgildk5un7ie5tz6fzywdd1dcryr',
+    client_id = 's4fwgb8isqexk2j87n2xagqfc3hhy6'
 }
 
 /**
- * @param {String} artist Artist name
- * @param {function(Error, Array)} cb Callback receives an array of Artist objects with given name or 
- * an Error if there is no Artist with given name.
+ * @param {function(Error, Array)} cb Callback receiving an array with top games or Error if not succeeded
  */
-function searchArtist(artist, cb) {
+function getTopGames(cb) {
+    urllib.request(IGDB_HOST, 
+        {
+            method: 'GET',
+            headers: {
+                'Client-ID' : 's4fwgb8isqexk2j87n2xagqfc3hhy6',
+                'Key' :'5tfgildk5un7ie5tz6fzywdd1dcryr'
+            },
+            data: {
+                'top_games': 'fields *; where rating > 75;',
+            }
+        },
+            (err, data, res) => {
+                console.log(res.status, res.headers);
+            }
+        );
+    
+}
 
+/**
+ * @param {String} game Game name
+ * @param {function(Error, Array)} cb Callback receives an array of Game objects with given name or 
+ * an Error if there is no Game with given name.
+ */
+function searchGame(game, cb) {
+    urllib.request(IGDB_HOST, 
+        {
+            method: 'GET',
+            headers: {
+                'Client-ID' : 's4fwgb8isqexk2j87n2xagqfc3hhy6',
+                'Key' :'5tfgildk5un7ie5tz6fzywdd1dcryr'
+            },
+            data: {
+                'game': 'fields *; where name = ' + game,
+            }
+        },
+            (err, data, res) => {
+                console.log(res.status, res.headers);
+            }
+        );
 }
 
 
-module.exports = {
-    'getTopTracks': getTopTracks
-}
---
+++ test/unitary/api_access.test.js
@@ -0,0 +1,7 @@
+const data = require('./igdb-data')
+
+test('gets the top games', () => {
+    expect(data.getTopGames(cb)).toBe()
+}
+
+);
